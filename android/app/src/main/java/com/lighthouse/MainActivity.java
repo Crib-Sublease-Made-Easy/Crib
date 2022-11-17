f03b0ed1129ed40af4b29c748e65bf1bb65afa7e
isaaclhy13 <hlee777@wisc.edu>
2022-11-16 21:44:28 -0600

    back

--- android/app/src/main/java/com/lighthouse/MainActivity.java
+++ android/app/src/main/java/com/lighthouse/MainActivity.java
@@ -15,6 +15,8 @@ public class MainActivity extends ReactActivity {
     return "lighthouse";
   }
 
+
+
   /**
    * Returns the instance of the {@link ReactActivityDelegate}. There the RootView is created and
    * you can specify the rendered you wish to use (Fabric or the older renderer).
