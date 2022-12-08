5fe560ba890099a1cae0fd6facc0f7e988e9060f
Hadeid Mirza <hadeidmirza04@gmail.com>
2022-12-07 20:55:44 -0600

    Report page

---
+++ Screens/Main/Profile/Setting/ReportUs/reportUsStyle.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components/native';
+
+import { HEIGHT, WIDTH, PRIMARYCOLOR, LIGHTGREY, MEDIUMGREY, EXTRALIGHT, DARKGREY } from '../../../../../sharedUtils'
+
+export const RowContainer = styled.View`
+    padding-vertical: ${HEIGHT*0.01}px
+    width: ${WIDTH*0.9}px;
+    align-self: center
+`
+
+export const CategoryName = styled.Text`
+    font-size: ${HEIGHT*0.0175}px
+    padding-vertical: ${HEIGHT*0.01}px
+    font-weight: 500
+`
+export const TitleContainer = styled.TextInput`
+    width: ${WIDTH*0.9}px;
+    background-color: ${EXTRALIGHT}
+    height: ${HEIGHT*0.05}px
+    padding-left: ${WIDTH*0.025}px
+    border-radius: 10px
+`
+
+export const DescriptionInput = styled.TextInput`
+    width: ${WIDTH*0.9}px;
+    height: ${HEIGHT*0.2}px;
+    border-radius: 10px
+    color: black
+    background-color: ${EXTRALIGHT}
+    padding-top: ${HEIGHT*0.01}px;
+    padding-horizontal: ${HEIGHT*0.02}px;
+    align-self:center
+`
